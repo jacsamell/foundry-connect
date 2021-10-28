3a133550db827144bfbc4da6a3fc13d583495007
Jacob Ellis <jacob@goellis.com>
2021-10-28 22:22:04 +0100

    Lambda saves a record with fake vanity numbers in dynamo

--- lib/foundary-connect-stack.ts
+++ lib/foundary-connect-stack.ts
@@ -1,18 +1,23 @@
 import * as cdk from '@aws-cdk/core';
 import { NodejsFunction, SourceMapMode } from '@aws-cdk/aws-lambda-nodejs';
-import { Table } from '@aws-cdk/aws-dynamodb';
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
 
 export class FoundaryConnectStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new NodejsFunction(this, 'vanity-lambda', {
+        const vanityLambda = new NodejsFunction(this, 'vanity-lambda', {
             entry: './lib/handlers/vanity.ts',
             functionName: 'vanity-lambda',
             bundling: {
                 sourceMap: true,
                 sourceMapMode: SourceMapMode.DEFAULT,
             },
-        })
+        });
+
+        new Table(this, 'vanity-table', {
+            partitionKey: { name: 'phoneNumber', type: AttributeType.STRING },
+            tableName: 'vanity-numbers'
+        }).grantWriteData(vanityLambda);
     }
 }
--- lib/handlers/vanity.ts
+++ lib/handlers/vanity.ts
@@ -1,10 +1,22 @@
 import { ConnectContactFlowHandler } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+const dynamo = new DynamoDB.DocumentClient();
 
 export const handler: ConnectContactFlowHandler = async (event) => {
     console.log('Received event', JSON.stringify(event, null, 4));
 
     const phoneNumber = event.Details.ContactData.CustomerEndpoint?.Address;
-    if (!phoneNumber) throw new Error("Can't find customer phone number");
+    if (!phoneNumber) {
+        throw new Error("Can't find customer phone number");
+    }
 
-    return { hello: phoneNumber };
+    const vanityNumbers = ['hello', 'world', phoneNumber];
+
+    await dynamo.put({
+        TableName: 'vanity-numbers',
+        Item: { phoneNumber: phoneNumber, vanityNumbers: vanityNumbers }
+    }).promise();
+
+    return { message: `Your possible vanity numbers are: ${vanityNumbers.join(' or, ')}` };
 }
